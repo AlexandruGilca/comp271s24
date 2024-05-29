4e3142447e521663d70d25bd33e574b5f63fc4d3
AlexandruGilca <agilca@luc.edu>
2024-05-29 16:03:15 +0000

    Work towards assignment 1

--- Assignments/Assignment 01/DynamicArray.java
+++ Assignments/Assignment 01/DynamicArray.java
@@ -4,5 +4,34 @@
  * YOUR WORK MAY BE LOST AND YOU MAY HAVE TO START ALL OVER AGAIN.
  */
 public class DynamicArray {
-    
+
+   private String[] data;
+
+    /** Currently available position in array data */
+   private int position; 
+
+   /*Constant with default size */
+
+   private static final int DEFAULT_SIZE = 10;
+
+   public DynamicArray(int size) {
+    this.data = new String[size];
+    this.position = 0;
+   } // basic constructor
+
+   /*default constructr */
+   public DynamicArray() {
+    this(DEFAULT_SIZE);
+   }// default constructor
+
+   public void add(String string) {
+        //make sure there is room in array data
+        if(this.position == this.data.lenght){ 
+            resize();
+        }
+            // Now array has room for more elements.
+        this.data[this.position] = string;
+        this.position++;    
+   }  // method add
+
 } // class DynamicArray
